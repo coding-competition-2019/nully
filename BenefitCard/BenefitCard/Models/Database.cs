c59129726ba839b4d948a46b9cb0b1551bae4e4e
spidoosho <hoang.dusan@gmail.com>
2019-12-04 13:06:00 +0100

    Who destroyed my db?!

--- BenefitCard/BenefitCard/Models/Database.cs
+++ BenefitCard/BenefitCard/Models/Database.cs
@@ -4,46 +4,58 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using BenefitCard.Models.JsonReader;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace BenefitCard.Models
 {
-	public class Database : DbContext
-	{
-		public DbSet<Facility> Facilities { get; set; }
-		//Dictionary<string,List<Facility>>
-		public DbSet<Tuple<string, List<Facility>>> Activities { get; set; }
+    public class Database : DbContext
+    {
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<Tuple<string, List<Facility>>> Activities { get; set; }
 
 
-        //TODO
         public Database()
         {
             LoadDatabase();
-
         }
 
-        //TODO
         public void LoadDatabase()
         {
+            int counter = 1;
             //- konzultovat s dušanem
             Reader reader = new Reader();
             reader.Read();
 
             foreach (Facility f in reader.facilities)
             {
+                f.Id = counter++;
                 Facilities.Add(f);
 
                 foreach (string activity in f.Activities)
                 {
-                    bool found = false;
-                    foreach (Tuple<string, List<Facility>> t in Activities)
-                    {
-                        if (t.Item1 == activity)
-                        {
-                            found = true;
-
-                        }
-                    }
+                    AddActivity(activity, f);
                 }
+            }
+        }
+
+        void AddActivity(string activity, Facility f)
+        {
+            bool found = false;
+            foreach (Tuple<string, List<Facility>> t in Activities)
+            {
+                if (t.Item1 == activity)
+                {
+                    found = true;
+                    t.Item2.Add(f);
+                }
+            }
+
+            if (found == false)
+            {
+                List<Facility> l = new List<Facility>();
+                l.Add(f);
+
+                Activities.Add(new Tuple<string, List<Facility>>(activity, l));
             }
         }
     }
