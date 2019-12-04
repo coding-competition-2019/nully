393bc66f8d364d9715050bf80d8524cbc0d68808
spidoosho <hoang.dusan@gmail.com>
2019-12-04 09:58:36 +0100

    Loaded data

--- BenefitCard/BenefitCard/Models/JsonReader/Reader.cs
+++ BenefitCard/BenefitCard/Models/JsonReader/Reader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace BenefitCard.Models.JsonReader
 {
     public class Reader
     {
-
+        string json = JsonConvert.SerializeObject(account, Formatting.Indented);
     }
 }
