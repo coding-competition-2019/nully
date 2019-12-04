9b6bd490f3ab01d154b1bfa2e67042203a57ea48
JanKleprlik <kleprlikjan@gmail.com>
2019-12-04 11:23:09 +0100

    Added coordinates

---
+++ BenefitCard/BenefitCard/Models/Coordinates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BenefitCard.Models
+{
+	public class Coordinates
+	{
+		public double Longtitude { get; set; }
+		public double Latitude { get; set; }
+	}
+}
